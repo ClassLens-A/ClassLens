b41118d3bdbfa0fa35c6f48b3ca98c9237330c03
Vyom Shah <vyom509shah@gmail.com>
2025-08-06 21:57:32 +0530

    Modified and Added .env

--- ClassLens_DB/ClassLens_DB/settings.py
+++ ClassLens_DB/ClassLens_DB/settings.py
@@ -11,9 +11,13 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import environ
+import os
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+env = environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 
 
 # Quick-start development settings - unsuitable for production
@@ -78,11 +82,11 @@ WSGI_APPLICATION = "ClassLens_DB.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "postgres",
-        "USER": "msu_spartans",
-        "PASSWORD": "Test@2211",
-        "HOST": "classlens-msu.postgres.database.azure.com",
-        "PORT": "5432",
+        "NAME": env("DB_NAME"),
+        "USER": env("DB_USER"),
+        "PASSWORD": env("DB_PASSWORD"),
+        "HOST": env("DB_HOST"),
+        "PORT": env("DB_PORT"),
     }
 }
 
