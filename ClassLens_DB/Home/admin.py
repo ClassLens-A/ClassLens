c7b8ac278498187045c7986a127424c3f07b167d
Yash Solanki <yashvsolanki81@gmail.com>
2025-08-06 17:42:49 +0530

    Added code field in subject and registered models in django admin

--- ClassLens_DB/Home/admin.py
+++ ClassLens_DB/Home/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Student, Subject, Teacher, Department, Enrollment, ClassSession,SubjectFromDept
+
+
+admin.site.register(Subject)
+
+admin.site.register(Department)
+
+admin.site.register(SubjectFromDept)
