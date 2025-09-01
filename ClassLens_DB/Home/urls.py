5f760ef901803be36c282af831666e79d7448bf9
Vyom Shah <vyom509shah@gmail.com>
2025-09-01 18:52:01 +0530

    Added get subjects endpoint

--- ClassLens_DB/Home/urls.py
+++ ClassLens_DB/Home/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from django.urls import include
-from Home.views import getDepartments,registerNewStudent,registerNewTeacher,validateStudent,validateTeacher
+from Home.views import getDepartments,registerNewStudent,registerNewTeacher,validateStudent,validateTeacher,get_subject_details
 
 urlpatterns=[
     path('getDepartments/', getDepartments, name='get_departments'),
@@ -8,4 +8,5 @@ urlpatterns=[
     path('registerNewTeacher',registerNewTeacher,name='register_new_teacher'),
     path('validateStudent',validateStudent,name='validate_student'),
     path('validateTeacher',validateTeacher,name='validate_teacher'),
+    path('getSubjectDetails',get_subject_details,name='get_subject_details'),
 ]
